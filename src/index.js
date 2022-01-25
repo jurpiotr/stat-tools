469c95d0d192759ce01173c2d1225a68eb1ae6af
jurpiotr <jurpiotr@gmail.com>
2022-01-25 23:24:01 +0100

    clear base view

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import './sass/index.scss';
 
 const wave = document.querySelector('.wave');
-const main = document.querySelector('main');
+
 let posY = 0;
 const waveH = {
    scrollPosition: 200,
@@ -13,9 +13,7 @@ const waveH = {
 }
 
 const alignDeviationReductor = (pos, output, origin) => {
-
    const result = origin - window.scrollY/(pos/(origin-output));
- //  console.log(result)
    return result;
 }
 
@@ -24,17 +22,12 @@ window.addEventListener('scroll', () => {
 
 })
 const scrollToTop = () => {
-
- //  wave.setAttribute('d', 'M-0,200 C230,180 116,24 500,120 L500.00,0.00 L0.00,0.00 Z');
-
    if(posY < window.scrollY && window.scrollY <= 200){
       let m1Height = alignDeviationReductor(waveH.scrollPosition, waveH.outputHeightWave, waveH.m);
       let c1Height = alignDeviationReductor(waveH.scrollPosition, waveH.outputHeightWave, waveH.c1);
       let c2Height = alignDeviationReductor(waveH.scrollPosition, waveH.outputHeightWave, waveH.c2);
       let c3Height = alignDeviationReductor(waveH.scrollPosition, waveH.outputHeightWave, waveH.c3);
 
-      console.log(window.scrollY/8)
-      console.log("+");
       wave.setAttribute('d', 
       `M-0,${m1Height} 
       C230,${c1Height} 116,${c2Height} 500,${c3Height} L500.00,0.00 L0.00,0.00 Z`);
@@ -42,9 +35,8 @@ const scrollToTop = () => {
    } else if(posY > window.scrollY && window.scrollY <= 200){
       wave.setAttribute('d', 
       `M-0,${posY + window.scrollY} C230,180 116,24 500,${window.scrollY} L500.00,0.00 L0.00,0.00 Z`);
-      console.log("-");
       posY = window.scrollY;
    }
-   console.log(wave)
+
  };
  
