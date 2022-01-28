d192f7f7d9d96669a1ddb73c9862fe127bc51922
jurpiotr <jurpiotr@gmail.com>
2022-01-28 23:52:38 +0100

    Positioning and setting the header background

--- src/index.js
+++ src/index.js
@@ -9,7 +9,11 @@ const waveH = {
    m: 100,
    c1: 180,
    c2: 24,
-   c3: 120
+   c3: 120,
+   mHeight: 0,
+   c1Height: 0,
+   c2Height: 0,
+   c3Height: 0
 }
 
 const alignDeviationReductor = (pos, output, origin) => {
@@ -23,16 +27,21 @@ window.addEventListener('scroll', () => {
 })
 const scrollToTop = () => {
    if(posY < window.scrollY && window.scrollY <= 200){
-      let m1Height = alignDeviationReductor(waveH.scrollPosition, waveH.outputHeightWave, waveH.m);
-      let c1Height = alignDeviationReductor(waveH.scrollPosition, waveH.outputHeightWave, waveH.c1);
-      let c2Height = alignDeviationReductor(waveH.scrollPosition, waveH.outputHeightWave, waveH.c2);
-      let c3Height = alignDeviationReductor(waveH.scrollPosition, waveH.outputHeightWave, waveH.c3);
+      waveH.mHeight = alignDeviationReductor(waveH.scrollPosition, waveH.outputHeightWave, waveH.m);
+      waveH.c1Height = alignDeviationReductor(waveH.scrollPosition, waveH.outputHeightWave, waveH.c1);
+      waveH.c2Height = alignDeviationReductor(waveH.scrollPosition, waveH.outputHeightWave, waveH.c2);
+      waveH.c3Height = alignDeviationReductor(waveH.scrollPosition, waveH.outputHeightWave, waveH.c3);
 
       wave.setAttribute('d', 
-      `M-0,${m1Height} 
-      C230,${c1Height} 116,${c2Height} 500,${c3Height} L500.00,0.00 L0.00,0.00 Z`);
+      `M-0,${waveH.mHeight} 
+      C230,${waveH.c1Height} 116,${waveH.c2Height} 500,${waveH.c3Height} L500.00,0.00 L0.00,0.00 Z`);
       posY = window.scrollY;
    } else if(posY > window.scrollY && window.scrollY <= 200){
+      console.log(
+         "window.scrollY: " + window.scrollY + '\n' +
+         "posY: " + window.scrollY + '\n' 
+      )
+
       wave.setAttribute('d', 
       `M-0,${posY + window.scrollY} C230,180 116,24 500,${window.scrollY} L500.00,0.00 L0.00,0.00 Z`);
       posY = window.scrollY;
