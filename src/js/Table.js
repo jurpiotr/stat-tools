1d8efdd0806b4cd63348735619a2185a2f8e65d0
jurpiotr <jurpiotr@gmail.com>
2022-02-04 23:38:03 +0100

    added action moving active tab into the table

--- src/js/Table.js
+++ src/js/Table.js
@@ -10,30 +10,27 @@ export class Table {
    show = () => {
       console.log(this.content)
       this.content.classList.add('table');
-      // this.content.innerHTML = `
-      // <p>sdfsdf</p>
-      // `
-      
    }
+
    start = (e) => {
       this.isStart = true;
       this.activeTab = e;
+      console.log
       this.startY = e.offsetTop;
       this.mouseY = window.event.pageY;
       e.classList.add('table__active');
-      e.innerText = `${this.isStart} +++ ${this.startY} + ${window.event.pageY}`;
-      // next add e.clientHeight + e.offsetTop for element 
+      e.innerText = `e.offsetTop: ${e.offsetTop} + window.event.pageY: ${window.event.pageY} e.clientHeight: ${e.clientHeight}`;
    }
    move = (e) => {
-      
-         if(!this.isStart && !this.activeTab.classList.contains('table__active')){ return }
-
-         this.activeTab.style.zIndex = 2;
-         console.log(this.isStart)
-         let mouseMove = this.mouseY - window.event.pageY;
-         this.activeTab.innerText = `${this.isStart} +++ ${this.startY} + ${mouseMove}`;
-         this.activeTab.style.transform = `translateY(${-mouseMove}px)`;
-         console.log(mouseMove)
+      if(!this.isStart && !this.activeTab?.classList.contains('table__active')){ return }
+      this.activeTab.style.zIndex = 1;
+      let mouseMoved = this.mouseY - window.event.pageY;
+      this.activeTab.style.transform = `translateY(${-mouseMoved}px)`;
+      this.activeTab.innerText = `diff move: ${-1*mouseMoved + this.activeTab.offsetTop} + next elem: ${this.activeTab.nextElementSibling.offsetTop}`;
+     
+      if(-1*mouseMoved + this.activeTab.offsetTop > this.activeTab.nextElementSibling.offsetTop){
+         this.activeTab.classList.add('table__row--active')
+      }
    }
    end = (e) => {
       this.isStart = false;
