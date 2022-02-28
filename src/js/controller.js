b5b72131428df51a98f753c45a1b44650b1dd314
jurpiotr <jurpiotr@gmail.com>
2022-02-28 20:10:18 +0100

    used API database for cards result

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,29 +1,36 @@
 import axios from "axios";
 
-const render = () => {
-   filmList.forEach(film => {
-      
+const render = (videoList) => {
+   const videoCards = document.getElementById('video-cards');
+   videoCards.innerHTML = '';
+      videoList.map((video) => {
+         videoCards.innerHTML += `
+         <div class="videos__video-card">
+         <button class="videos__btn-add-card name="${video.id}">+Watchlist</button>
+         <img class="videos__img-card"src="${video.show.image.medium}" alt="${video.show.name}">
+            <p class="videos__title-card">${video.show.name}</p>
+      </div>
+         `
    });
+   return videoCards;
 }
 
 export const tv = async () => {
+   let importantData = [];
 
-      const values = await axios({
-         url: `https://api.tvmaze.com/schedule`,
-         method: "GET",
-         headers: {
-            Accept: 'application/json',
-         }
-      })
-         .then((response) => {
-            const filmList = response.data;
-            return filmList;
-         })
-         .then((dataJSON) => {
-            
-            console.log(dataJSON)
-            
-         });
-      return values;
+   const values = await axios({
+      url: `https://api.tvmaze.com/schedule`,
+      method: "GET",
+      headers: {
+         Accept: 'application/json',
+      }
+   })
+   .then((response) => {
+      return response.data;
+   })
+   .then((data) => {
+      render(data);
+   })
 
+   return values;
 }
